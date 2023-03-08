f4dc4497c24e2372d802fd16bea03dfd976885b6
glaukopacheco <glaukopacheco@gmail.com>
2023-03-08 19:09:07 -0300

    Projeto base

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+    <>
+      <main className=" h-screen flex justify-center items-center">
+        
+        <h1 className="text-3xl font-bold">
+          Home
+        </h1>
+
+      </main>
+    </>
+  )
+}
---
+++ src/pages/sobre.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Sobre() {
+  return (
+    <main className=" h-screen flex justify-center items-center">
+        
+        <h1 className="text-3xl font-bold">
+          Sobre
+        </h1>
+
+      </main>
+  )
+}
